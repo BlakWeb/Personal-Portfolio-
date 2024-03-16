741ab0d9eceebb5d74f246ceea9b0f2435e477b8
BlakWeb <144697255+BlakWeb@users.noreply.github.com>
2024-03-16 23:19:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+    var dots = elem.getAttribute('data-dots');
+    var marked = elem.getAttribute('data-percent');
+    var percent = Math.floor((dots * marked) / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+    }
+
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++){
+        pointsMarked[i].classList.add('marked');
+    }
+});
+
+let typed = new Typed(".text", {
+    strings: [" A", "FrontEnd Developer", "Githuber", "Web Designer."],
+    typeSpeed: 80,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+
